8e9b97f8b011a11429209dcea4f8c8fee0128425
esamsalh <86736402+esamsalh@users.noreply.github.com>
2026-06-20 17:01:32 +0200

    حذف

--- 12/admin/includes/footer.php
+++
@@ -1,9 +0,0 @@
-    </div>
-</main>
-<footer class="admin-footer">
-    <div class="admin-container">
-        <p>&copy; <?= date('Y') ?> <?= SITE_NAME ?> Admin Panel. All rights reserved.</p>
-    </div>
-</footer>
-</body>
-</html>
