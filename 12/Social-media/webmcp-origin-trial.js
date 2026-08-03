7158c35f971d8e0ef3d6ba2955c330c43fb91020
esamsalh <86736402+esamsalh@users.noreply.github.com>
2026-08-03 04:37:57 +0200

    up

---
+++ 12/Social-media/webmcp-origin-trial.js
@@ -0,0 +1,9 @@
+(() => {
+    'use strict';
+
+    const token = 'A/Pfugc8OkTEsFaOfkzLZN8El+Gcl36zWbagzuZC5l4IhJBRpmUbOVN+Y0YopvBkRbyPO5i5v9wK04qq6q1P2g8AAABjeyJvcmlnaW4iOiJodHRwczovL3d3dy50b29scmFyLmNvbTo0NDMiLCJmZWF0dXJlIjoiV2ViTUNQIiwiZXhwaXJ5IjoxNzk0ODczNjAwLCJpc1RoaXJkUGFydHkiOnRydWV9';
+    const meta = document.createElement('meta');
+    meta.httpEquiv = 'origin-trial';
+    meta.content = token;
+    document.head.appendChild(meta);
+})();
